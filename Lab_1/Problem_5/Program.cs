9c522002d58c42b7302169d8fc2f4fa394a77f4c
Andrew Kulchickii <droppet228@gmail.com>
2022-09-05 17:12:56 +0300

    Lab 1

---
+++ Lab_1/Problem_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Problem_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte first = -100;
+            byte second = 128;
+            short third = -3540;
+            ushort fourth = 64876;
+            uint fifth = 2147483648;
+            int sixth = -1141583228;
+            long seventh = -1223372036854775808;
+
+            Console.WriteLine(
+                $"first: {first}\n" +
+                $"second: {second}\n" +
+                $"third: {third}\n" +
+                $"fourth: {fourth}\n" +
+                $"fifth: {fifth}\n" +
+                $"sixth: {sixth}\n" +
+                $"seventh: {seventh}");
+        }
+    }
+}
---
+++ Lab_1/Problem_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Problem_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal first = 3.141592653589793238M;
+            float second = 1.60217657F;
+            decimal third = 7.8184261974584555216535342341M;
+
+            Console.WriteLine(
+                $"first: {first}\n" +
+                $"{second}\n" +
+                $"{third}\n");
+        }
+    }
+}
